5873f17814f0d8ffefe8b4161057c7ea767b96a8
clarkohw <clark_oh-willeke@brown.edu>
2021-03-06 21:23:15 -0500

    privacy

--- backend/models.js
+++ backend/models.js
@@ -1,70 +1,66 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const { DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD } = require('./config');
+const { Sequelize, DataTypes } = require("sequelize");
+const { DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD } = require("./config");
 
-// const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-//     host: DB_HOST,
-//     dialect: 'postgres',
-// });
-const sequelize = new Sequelize('lonliness', 'clark2', 'password', {
-    host: 'localhost',
-    dialect: 'postgres',
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: "postgres",
 });
 
-const User = sequelize.define('User', {
-    id: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        primaryKey: true,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    password: {
-        type: DataTypes.STRING(60),
-        allowNull: false,
-    }
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING(60),
+    allowNull: false,
+  },
 });
 
-const Request = sequelize.define('Request', {
-    id: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        primaryKey: true,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    message: {
-        type: DataTypes.STRING,
-    },
+const Request = sequelize.define("Request", {
+  id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  message: {
+    type: DataTypes.STRING,
+  },
 });
 
-const Reply = sequelize.define('Reply', {
-    id: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        primaryKey: true,
-    },
+const Reply = sequelize.define("Reply", {
+  id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    primaryKey: true,
+  },
 });
 
-const Suggestion = sequelize.define('Suggestion', {
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    name: {
-        type: DataTypes.STRING,
-    },
-    phone: {
-        type: DataTypes.STRING(10),
-    },
-    message: {
-        type: DataTypes.STRING,
-    }
+const Suggestion = sequelize.define("Suggestion", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+  },
+  phone: {
+    type: DataTypes.STRING(10),
+  },
+  message: {
+    type: DataTypes.STRING,
+  },
 });
 
 User.hasMany(Request);
@@ -79,4 +75,4 @@ Reply.belongsTo(Request);
 Reply.hasMany(Suggestion);
 Suggestion.belongsTo(Reply);
 
-module.exports = { sequelize, Sequelize, User, Request, Reply, Suggestion }
+module.exports = { sequelize, Sequelize, User, Request, Reply, Suggestion };
