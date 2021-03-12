37c2c45776b3ebe614c29e273ad4c0fc183852c9
Nicholas Springer <nicholas.m.springer@gmail.com>
2021-03-12 00:01:48 -0800

    error messages tweaks

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -84,12 +84,12 @@ router.post(
         }
 
         if (user == null) {
-            return res.sendStatus(StatusCodes.UNAUTHORIZED);
+            return res.status(StatusCodes.UNAUTHORIZED).send('Invalid id or password');
         }
 
         try {
             if (!(await bcrypt.compare(req.body.password, user.password))) {
-                return res.sendStatus(StatusCodes.UNAUTHORIZED);
+                return res.status(StatusCodes.UNAUTHORIZED).send('Invalid id or password');
             }
         } catch (err) {
             console.log(err);
